d79885447267b82e5a4106b50753e7f6406deb23
omniscient <17525998+omnisci3nce@users.noreply.github.com>
2021-02-13 23:27:09 +1100

    finish tutorial

--- lambda_calc_ocaml.ml
+++ lambda_calc_ocaml.ml
@@ -31,7 +31,9 @@ let rec tokenise (text: char list) =
 type term =
   | VariableT     of char
   | LambdaT       of char * term
+  | ClosureT      of char * term * environment
   | ApplicationT  of term * term
+and environment = (char * term) list
 
 let rec parseSingle (tokens: token list): (term * token list) =
   match tokens with
@@ -42,29 +44,57 @@ let rec parseSingle (tokens: token list): (term * token list) =
   | LParen::code ->
       let fn, afterFirst = parseSingle code in
       let value, afterValue = parseSingle afterFirst in
-
+      begin
       match afterValue with
       | RParen::rest ->
           ApplicationT (fn, value), rest
       | _ -> failwith "expected a right paren lol"
+      end
   | _ -> failwith "bad parse"
 
 
 let parse (tokens: token list) =
   fst (parseSingle tokens)
 
-type Environment = (char * term) list
 
-let evalInEnv env term =
+let rec evalInEnv env (term: term) : term =
   match term with
   | VariableT name ->
-      match List.find_opt (fun a -> a = name) env with
-        | Some term -> term
+      begin
+      match List.find_opt (fun (c, t) -> c = name) env with
+        | Some (_, term) -> term
         | None -> failwith "couldnt find a term by name"
-  | LambdaT (arg, body) ->
-      ClosureT (arg, body, env)
+      end
+  | LambdaT (arg, body) -> ClosureT (arg, body, env)
   | ApplicationT (fn, value) ->
+      begin
+      match evalInEnv env fn with
+        | ClosureT (arg, body, closedEnv) ->
+            let evaluatedValue = evalInEnv env value in
+            let newEnv = (arg, evaluatedValue)::closedEnv @ env in
+            evalInEnv newEnv body
+        | _ ->
+            failwith "cannot apply what was given"
+      end
+  | closure -> closure
 
 let eval (term: term): term =
   evalInEnv [] term
+
+let rec pretty (term: term): char list =
+  match term with
+  | VariableT name -> [name]
+  | LambdaT (arg, body) -> ['\\'; arg; '.'] @ pretty body
+  | ClosureT (arg, body, _) -> ['\\'; arg; '.'] @ pretty body
+  | ApplicationT (fn, value) -> '(' :: pretty fn @ [' '] @ pretty value @ [')']
+
+let interpret (characters: char list) : char list =
+  characters
+    |> tokenise
+    |> parse
+    |> eval
+    |> pretty
+
+let interpretString s =
+  s |> String.to_seq |> List.of_seq |> interpret |> List.to_seq |> String.of_seq
 
